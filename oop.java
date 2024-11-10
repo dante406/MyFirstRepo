83488aad946ac081306516e758768ccccbd9f45c
dante406 <jumperguyk10@gmail.com>
2024-11-10 20:44:01 +0600

    latest

---
+++ oop.java
@@ -0,0 +1,19 @@
+public class oop {
+    private int age;
+    private double cgpa;
+    private String name;
+
+
+    public void setAge(int age){
+        this.age = age;
+    }
+    public  void setName(String name){
+        this.name = name;
+    }
+    public void setCgpa(double cgpa){
+        this.cgpa = cgpa;
+    }
+    public String getName (){
+        return name;
+    }
+}
---
+++ oop_Driver.java
@@ -0,0 +1,7 @@
+public class oop_Driver {
+    public static void main(String[] args) {
+        oop user1 = new oop();
+        user1.setName("Anonto");
+        System.out.println(user1.getName());
+    }
+}
