1d6fb57606755905096202d9b3381841dfd39884
dante406 <33100379+dante406@users.noreply.github.com>
2025-10-07 02:45:00 +0600

    hi 3

---
+++ Node/src/Node/Node.java
@@ -0,0 +1,31 @@
+package Node;
+
+public class Node {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		school T1=new school();
+		school T2=new school();
+		school T3=new school();
+		T1.ID=5646;
+		T1.name="Mashrur";
+		T1.num=192;
+		T1.address="House 30";
+		T2.ID=5646;
+		T2.name="Mashrur";
+		T2.num=192;
+		T2.address="House 30";
+		
+		
+		T3.ID=5646;
+		T3.name="Mashrur";
+		T3.num=192;
+		T3.address="House 30";
+		T1.Display();
+		T2.Display();
+		T3.Display();
+		
+	}
+	
+
+}
---
+++ Node/src/Node/school.java
@@ -0,0 +1,11 @@
+package Node;
+
+public class school {
+	int ID,num;
+	String name,address;
+	void Display()
+	{
+		System.out.println("Name,ID,number and address ="+name+" "+ID+" "+num+" "+address);
+		
+	}
+}
